995ea65177b1ad1dde835c53d65f5734e8b8d3e7
Crypto Zealot <34049724+cryptozealot@users.noreply.github.com>
2019-07-04 15:29:16 +0300

    Create 004.py

---
+++ 004.py
@@ -0,0 +1,16 @@
+def is_palindrome(n):
+    n = str(n)
+    if n == n[::-1]:
+        return True
+    return False
+
+
+result = 0
+for i in range(999,900,-1):
+    for j in range(999,900,-1):
+        if is_palindrome(i*j) and i*j > result:
+            result = i*j # product
+            print(i) # first number
+            print(j) # second number
+
+print(result)
