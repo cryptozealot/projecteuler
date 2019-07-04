a723929f8312f732365682e41a68b5d366ecdb90
Crypto Zealot <34049724+cryptozealot@users.noreply.github.com>
2019-07-04 16:18:43 +0300

    Update 005.py
    reduced accuracy and improved speed

--- 005.py
+++ 005.py
@@ -2,14 +2,15 @@
 
 
 def devisible_by_1_to_20(n):
-    for i in range(3,21):
+    primes = [7,9,11,13,16,17,19]
+    for i in primes:
         if n % i != 0:
             return False
     return True
 
-number = 20
+number = 2000
 while True:
     if devisible_by_1_to_20(number):
         print(number)
         break
-    number +=20
+    number +=160 #reduce for accuracy
